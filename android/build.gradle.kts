01d21e2bef0e76304aec60be6685adf739f7a21b
phamvinh203 <vinhnd203@gmail.com>
2026-09-15 21:34:33 +0700

    build(android): upgrade compileSdk to 35 and configure subproject flutter properties

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,17 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+    if (project.name != "app") {
+        project.extra.set(
+            "flutter",
+            mapOf(
+                "compileSdkVersion" to 35,
+                "minSdkVersion" to 21,
+                "targetSdkVersion" to 34,
+                "ndkVersion" to "27.0.12077973"
+            )
+        )
+    }
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
